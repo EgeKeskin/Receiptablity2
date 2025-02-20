e22fb73e0ca79592fb36acbf79c001d4482d4578
Nico Stuart <n.stuart@wustl.edu>
2025-02-19 18:47:14 -0600

    Merged all UI updates into this  (the page-testing-links.html now is the most up to date).

--- core/urls.py
+++ core/urls.py
@@ -12,4 +12,7 @@ urlpatterns = [
     path('in-room/', in_room, name='in-room'),
     path('payment/', payment, name='payment'),
     path('page-testing-links/', page_testing_links, name='page-testing-links'), 
+    path('upload/', upload_receipt_view, name='upload_receipt'),
+    path('receipt/<uuid:receipt_id>/', receipt_room_view, name='receipt_room'),
+    path('upload-receipt/', upload_receipt_view, name='upload_receipt_view'),
 ]
