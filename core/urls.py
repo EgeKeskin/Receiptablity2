dde503201e575f11d4c7d1eb73d38ec998be6613
EgeKeskin <e.keskin@wustl.edu>
2025-03-03 21:53:11 -0600

    register stuff

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', home, name='home'),
@@ -11,4 +12,16 @@ urlpatterns = [
     path('create-room/', create_room, name='create-room'),
     path('in-room/', in_room, name='in-room'),
     path('payment/', payment, name='payment'),
+    path('password_reset/', 
+         auth_views.PasswordResetView.as_view(template_name='password_reset_form.html'), 
+         name='password_reset'),
+    path('password_reset/done/', 
+         auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), 
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', 
+         auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), 
+         name='password_reset_confirm'),
+    path('reset/done/', 
+         auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), 
+         name='password_reset_complete'),
 ]
